0e4ac45709f24b7da7433354628b2e9c6de56adc
Bransholm <kenneth@bransholm.dk>
2023-04-26 16:25:22 +0200

    Modal window updated with full data

--- app.js
+++ app.js
@@ -37,8 +37,20 @@ function addCharacters(characters) {
     .addEventListener("click", characterClicked);
   function characterClicked() {
     document.querySelector("dialog").showModal();
-    document.querySelector("#dialogimage").src = characters.image;
-    document.querySelector("#dialogname").textContent = characters.name;
-    console.log("Her er vi");
+    document.querySelector("#dialog-name").textContent = characters.name;
+    document.querySelector("#dialog-image").src = characters.image;
+    document.querySelector("#dialog-name-normal").textContent = `Name: ${characters.name}`;
+    document.querySelector("#dialog-gender").textContent = `Gender: ${characters.gender}`;
+    document.querySelector("#dialog-age").textContent = `Age: ${characters.age}`;
+    document.querySelector("#dialog-hair-color").textContent = `Hair color: ${characters.haircolor}`;
+    document.querySelector("#dialog-nickname").textContent = `Nickname: ${characters.nickname}`
+    document.querySelector("#dialog-occupation").textContent = `Occupation: ${characters.occupation}`;
+    document.querySelector("#dialog-voicedBy").textContent = `Voiced by: ${characters.voicedBy}`;
+    document.querySelector("#dialog-religion").textContent = `Religion: ${characters.religion}`;
+    document.querySelector("#dialog-catchPhrase").textContent = `Catchphrase ${characters.catchPhrase}`;
+    document.querySelector("#dialog-school-grade").textContent = `School grade: ${characters.schoolGrade}`;
+    document.querySelector("#dialog-episodes").textContent = `Episodes: ${characters.episodes}`;
+    document.querySelector("#dialog-appearances").textContent = `Appearances: ${characters.appearances}`;
+    document.querySelector("#dialog-first-appearance").textContent = `First appearance: ${characters.firstAppearance}`;  
   }
 }
